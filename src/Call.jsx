f9b0b8dc29fadc7ed46f4a3bb3d765a1ad961b1d
KaiserAccount <brandynmckeen20@gmail.com>
2024-09-14 15:21:24 -0400

    vase

---
+++ src/Call.jsx
@@ -0,0 +1,13 @@
+// Call.jsx
+import { h } from 'preact';
+
+const Call = () => {
+  return (
+    <div>
+      <h1>Call Page</h1>
+      {/* Add call UI heres */}
+    </div>
+  );
+};
+
+export default Call;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,28 @@
-import { render } from 'preact'
-import { App } from './app.jsx'
-import './index.css'
+import { h, render } from 'preact';
+import { Router, Route } from 'preact-router';
 
-render(<App />, document.getElementById('app'))
+// import { Firestore, collection, addDoc } from '@firebase/firestore';
+
+// // import firebase from 'firebase/app';
+// // import { getAnalytics, logEvent, setCurrentScreen } from 'firebase/analytics'; // From the same source
+// import { logEvent } from '@firebase/analytics'; // Assuming you exported 'analytics'
+// import { analytics } from './firebase.js'; // Assuming you exported 'analytics'
+
+// const firestore = new Firestore();
+
+// logEvent(analytics, 'screen_view', { screen_name: 'doctor_profile' });
+
+import Call from './Call.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Route path="/Call" component={Call} />
+
+      </Router>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('app'));
